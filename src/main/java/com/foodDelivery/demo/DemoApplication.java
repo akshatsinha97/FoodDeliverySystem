3de06ad42005e66516bb92190c234c4981249363
akshat <akshatsinha97@gmail.com>
2022-12-20 01:44:53 +0530

    sending admin user from data.sql

--- src/main/java/com/foodDelivery/demo/DemoApplication.java
+++ src/main/java/com/foodDelivery/demo/DemoApplication.java
@@ -3,30 +3,19 @@ package com.foodDelivery.demo;
 //import com.foodDelivery.demo.Model.Role;
 //import com.foodDelivery.demo.Model.User;
 import com.foodDelivery.demo.Entity.User;
+import com.foodDelivery.demo.Repository.RoleRepository;
 import com.foodDelivery.demo.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 import java.util.ArrayList;
 
 @SpringBootApplication(scanBasePackages = {"com.foodDelivery.demo.*"})
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-	@Bean
-	CommandLineRunner run(UserService userService){
-		return args -> {
-			userService.saveUser(new User(1, "Akshat", "Sinha", "akshat97", "akshatsinha97@gmail.com", "12345", new ArrayList<>()));
-		};
-	}
-
-
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,6 @@
 INSERT INTO roles (role_id, name) VALUES (1, "Admin");
 INSERT INTO roles (role_id, name) VALUES (2, "User");
 
+INSERT INTO user (id, email, first_name, last_name, password, username) VALUES (1, "akshatsinha97@gmail.com", "Akshat", "Sinha","$2a$10$sYz0n1EoN64DRCgmQ03uMOX.jwtNo8QAK9v2E3G4ekC1KB8NAz8TW" , "akshat97");
+
 INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
